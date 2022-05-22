ce99334545e51ef7a52be742884c173fd74443dd
Bruno Marchiori <bemarchiori@gmail.com>
2022-05-22 17:47:26 +0000

    criacao dim_customer

--- models/marts/dim_customer.sql
+++ models/marts/dim_customer.sql
@@ -1,14 +1,70 @@
 with stage_store as (
     select 
     s.businessentityid
+    ,row_number() over (order by s.businessentityid) as store_sk
     ,s.name as nome_loja
     ,'ST' as tipo
     from {{ref('stg_store')}} s
+    order by s.businessentityid
 ),
-stage_person as (
+dim_person as (
     select
-    *
-    from {{ref('dim_person')}} s
+    d.businessentityid
+    ,d.person_sk
+    ,d.full_name as nome_pessoa
+    ,d.tipo as tipo
+    from {{ref('dim_person')}} d
+),
+dim_salesterritory as (
+    select 
+    d.sales_territory_sk
+    ,d.territoryid
+    ,d.territory_name
+    ,d.countryregioncode
+    ,d.group
+    from {{ref('dim_salesterritory')}} d
+),
+stage_customer as (
+    select 
+    s.customerid
+    ,s.personid
+    ,s.storeid
+    ,s.territoryid
+    from {{ref('stg_customer')}} s
+    order by s.customerid
+),
+union_customers as (
+    select 
+    s.customerid
+    ,ss.nome_loja as customer_name
+    ,st.territory_name 
+    from stage_customer s
+    left join dim_salesterritory st on st.territoryid=s.territoryid
+    left join stage_store ss on ss.businessentityid = s.storeid
+    where s.storeid is not null
 
+    union all
+
+    select 
+    s.customerid
+    ,p.nome_pessoa as customer_name
+    ,st.territory_name 
+    from stage_customer s
+    left join dim_salesterritory st on st.territoryid=s.territoryid
+    left join dim_person p on p.businessentityid=s.personid
+    where s.storeid is null
+
+    order by customerid
+),
+transformed as (
+    select 
+    row_number() over (order by s.customerid) as customer_sk
+    ,s.customerid
+    ,s.customer_name
+    ,s.territory_name
+    
+    from union_customers s
+    order by s.customerid
 )
-select * from stage_person
+select * from transformed
+order by 1
