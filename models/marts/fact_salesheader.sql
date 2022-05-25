e2f68b9c0f37b254fa5176d8888f1bd86c95f627
Bruno Marchiori <bemarchiori@gmail.com>
2022-05-25 18:20:38 +0000

    finalizacao da fact_sales

--- models/marts/fact_salesheader.sql
+++ models/marts/fact_salesheader.sql
@@ -16,6 +16,25 @@ with stage_sales_order_header as (
     ,s.freight
     from {{ref('stg_salesorderheader')}} s
 ),
+stage_sales_order_detail as (
+    select 
+    s.salesorderid
+    ,s.salesorderdetailid
+    ,s.orderqty
+    ,s.productid
+    ,s.unitprice
+    ,s.unitpricediscount
+
+    from {{ref('stg_salesorderdetail')}} s
+ 
+),
+dim_product as (
+    select 
+    d.product_sk as product_fk
+    ,d.productid
+    from {{ref('dim_product')}} d
+
+),
 dim_customer as (
     select d.customer_sk as customer_fk
     ,d.customerid
@@ -46,12 +65,14 @@ dim_sales_reason as (
 sales_header_fact as (
     select
     s.salesorderid as sales_order_id
+    ,detail.salesorderdetailid as sales_order_detail_id
     ,customer.customer_fk
     ,shipaddress.address_fk as ship_address_fk
     ,billaddress.address_fk as bill_address_fk
     ,creditcard.creditcard_fk
     ,territory.sales_territory_fk
     ,reason.sales_reason_fk
+    ,product.product_fk
     ,s.orderdate
     ,s.duedate
     ,s.shipdate
@@ -63,10 +84,15 @@ sales_header_fact as (
     when 5 then 'Shipped'
     when 6 then 'Cancelled' end as status
     ,s.onlineorderflag
+    ,detail.orderqty as order_quantity
+    ,detail.unitprice as unit_price
+    ,detail.unitpricediscount as unit_price_discount
     ,subtotal
     ,taxamt
     ,freight
     from stage_sales_order_header s
+    left join stage_sales_order_detail detail on detail.salesorderid=s.salesorderid
+    left join dim_product product on product.productid = detail.productid
     left join dim_customer customer on customer.customerid = s.customerid
     left join dim_creditcard creditcard on creditcard.creditcardid = s.creditcardid
     left join dim_address billaddress on billaddress.addressid = s.billtoaddressid
